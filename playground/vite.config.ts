d988464b246840d736b351482f0fa2d49139050c
haixin <1875934025@qq.com>
2024-03-12 20:18:14 +0800

    fix: 11

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -97,10 +97,7 @@ export default defineConfig({
         main: path.resolve(__dirname, "index.html"),
       },
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id.toString().split("node_modules/")[1].split("/")[0].toString();
-          }
+        manualChunks: {
         },
         chunkFileNames: "static/js/[name]-[hash].js",
         entryFileNames: "static/js/[name]-[hash].js",
